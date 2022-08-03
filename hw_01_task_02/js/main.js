51c74ef3f7af0e3c7f2cffcc4b6cd0439525beec
Konstantin Lobanov <39191118+lobanov-k@users.noreply.github.com>
2022-08-03 21:33:59 +0300

    Update main.js

--- hw_01_task_02/js/main.js
+++ hw_01_task_02/js/main.js
@@ -1,6 +1,8 @@
 const firstValue = prompt("Enter your first value");
 const secondValue = prompt("Enter your second value");
 
-const res = (firstValue == secondValue) ? true : false;
+const res = firstValue == secondValue;
+// no need to use ? operator here. (firstValue == secondValue) will return Boolean by itself
+// alert converts variables to string. Boolean converts to "true" / "false" string.
 alert(res);
 
